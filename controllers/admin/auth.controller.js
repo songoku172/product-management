28c3d6e44644bfab981fc5656fd679632e04c4de
nguyenmanhquan <qn227217@gmail.com>
2025-03-03 22:10:54 +0700

    logout

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -3,16 +3,19 @@ const Role = require("../../model/role.model");
 var md5 = require('md5');
 const systemConfig = require("../../config/system");
 // [GET] /admin/auth/login
-
 module.exports.login= (req, res) => {   
-    res.render("admin/pages/auth/login",{
-        pageTitle : " trang đăng nhập " 
-    });
+    if(req.cookies.token) {
+        res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+    } else {
+        res.render("admin/pages/auth/login",{
+            pageTitle : " trang đăng nhập " 
+        });
+    }
+    
 
 };
 
 // [POST] /admin/auth/login
-
 module.exports.loginPost= async (req, res) => {   
     const email = req.body.email;
     const password = req.body.password;
@@ -41,5 +44,13 @@ module.exports.loginPost= async (req, res) => {
     };
     res.cookie("token", user.token);
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+};
+
+// [GET] /admin/auth/logout
+
+module.exports.logout= (req, res) => {   
+    // xóa token trong cookie
+    res.clearCookie("token");
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
 };
 
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,19 +6,23 @@ const productsCategoryRoute   = require("./products-category.route");
 const roleRoute    = require("./role.route");
 const accountRoute    = require("./account.route");
 const authRoute    = require("./auth.route");
+const authMiddleware = require("../../middlewares/auth.middleware");
 
 module.exports = (app) =>{
   const PATH_ADMIN = systemConfig.prefixAdmin;
   
-  app.use(PATH_ADMIN + '/dashboard',dashboardRoute );
+  app.use(PATH_ADMIN + '/dashboard',
+    authMiddleware.requireAuth,
+    dashboardRoute
+  );
 
-  app.use(PATH_ADMIN + '/products',productsRoute);
+  app.use(PATH_ADMIN + '/products',authMiddleware.requireAuth,productsRoute);
 
-  app.use(PATH_ADMIN + '/products-category',productsCategoryRoute);
+  app.use(PATH_ADMIN + '/products-category',authMiddleware.requireAuth,productsCategoryRoute);
 
-  app.use(PATH_ADMIN + '/roles',roleRoute);
+  app.use(PATH_ADMIN + '/roles',authMiddleware.requireAuth,roleRoute);
 
-  app.use(PATH_ADMIN + '/accounts',accountRoute);
+  app.use(PATH_ADMIN + '/accounts',authMiddleware.requireAuth,accountRoute);
 
   app.use(PATH_ADMIN + '/auth',authRoute);
        
