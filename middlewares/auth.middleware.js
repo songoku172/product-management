03955f8b166f9ff34dfba2e3b0b435a77408a646
nguyenmanhquan <qn227217@gmail.com>
2025-03-04 00:43:47 +0700

    private route

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require("../model/account.model");
+const Role = require("../model/role.model");
 const systemConfig = require("../config/system");
 
 module.exports.requireAuth =  async (req,res ,next) => {
@@ -7,10 +8,17 @@ module.exports.requireAuth =  async (req,res ,next) => {
     } else {
         const user = await Account.findOne({
             token : req.cookies.token
-        });
+        }).select("-password");
             if(!user){
                 res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
             } else {
+                // nếu tồn tại tài khoản thì sẽ trả ra data của tài khoản
+                const role = await Role.findOne({
+                    _id : user.role_id
+                }).select("title permissions");
+
+                res.locals.user = user;
+                res.locals.role = role;
                 next();
             }
     }
